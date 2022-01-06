cba1373969e37be2d81b6733be16d01d4ae0f32e
FredTsang <979738295@qq.com>
2022-01-06 10:41:35 +0800

    feat: intersection-of-two-linked-lists

---
+++ intersection-of-two-linked-lists.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  var p = headA, q = headB;
+
+  while (p != q) {
+    p = p == null ? headB : p.next;
+    q = q == null ? headA : q.next;
+  }
+
+  return p;
+};
