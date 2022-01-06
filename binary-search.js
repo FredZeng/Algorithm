0d2bae2762c81197911bee828a3bddc4d1829418
FredTsang <979738295@qq.com>
2022-01-06 22:51:08 +0800

    feat: binary-search

---
+++ binary-search.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    var mid = left + ((right - left) >> 1);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
