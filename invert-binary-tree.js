73585797a11ea8c1db44a5c39457ae490b1e6dc8
FredTsang <979738295@qq.com>
2022-01-07 23:39:59 +0800

    feat: invert-binary-tree

---
+++ invert-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) {
+    return root;
+  }
+
+  var left = root.left;
+  var right = root.right;
+
+  root.right = left;
+  root.left = right;
+
+  invertTree(left);
+  invertTree(right);
+
+  return root;
+};
