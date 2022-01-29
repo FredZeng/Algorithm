4db848d3b24c32a9e9afc91a11551ce9d1446ea6
FredTsang <979738295@qq.com>
2022-01-29 09:53:31 +0800

    feat: permutations

--- permutations.js
+++ permutations.js
@@ -3,21 +3,51 @@
  * @return {number[][]}
  */
 var permute = function (nums) {
-  if (nums.length == 1) {
-    return [nums];
-  } else if (nums.length == 2) {
-    return [nums, [nums[1], nums[0]]];
-  }
+  var res = [];
+  var len = nums.length;
 
-  let res = [];
+  var backtrace = function (nums, track) {
+    if (track.length === len) {
+      res.push([...track]);
+      return;
+    }
 
-  for (let i = 0, len = nums.length; i < len; i++) {
-    const top = nums.pop();
+    for (let i = 0; i < len; i++) {
+      var num = nums[i];
 
-    res.push(...permute(nums).map((it) => [top, ...it]));
+      if (track.indexOf(num) > -1) {
+        continue;
+      }
 
-    nums.unshift(top);
-  }
+      track.push(num);
+
+      backtrace(nums, track);
+
+      track.pop();
+    }
+  };
+
+  backtrace(nums, []);
 
   return res;
 };
+
+// var permute = function (nums) {
+//   if (nums.length == 1) {
+//     return [nums];
+//   } else if (nums.length == 2) {
+//     return [nums, [nums[1], nums[0]]];
+//   }
+
+//   let res = [];
+
+//   for (let i = 0, len = nums.length; i < len; i++) {
+//     const top = nums.pop();
+
+//     res.push(...permute(nums).map((it) => [top, ...it]));
+
+//     nums.unshift(top);
+//   }
+
+//   return res;
+// };
