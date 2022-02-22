bac058f5704fb6729b287f4f74c40f95cae115d9
FredTsang <979738295@qq.com>
2022-02-22 18:57:28 +0800

    feat: unique-paths

---
+++ unique-paths.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  let arr = new Array(m);
+  for (let i = 0; i < m; i++) {
+    arr[i] = new Array(n);
+  }
+  arr[0][0] = 1;
+
+  for (let i = 1; i < m; i++) {
+    arr[i][0] = 1;
+  }
+
+  for (let i = 1; i < n; i++) {
+    arr[0][i] = 1;
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+    }
+  }
+
+  return arr[m - 1][n - 1];
+};
