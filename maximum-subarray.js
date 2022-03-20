088dd6c36e1138a9cb2b6efa8cd5f0bfd08960f6
FredTsang <17154608+FredZeng@users.noreply.github.com>
2022-03-20 23:41:18 +0800

    feat: update maximum-subarray

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -5,16 +5,29 @@
 var maxSubArray = function (nums) {
   if (nums.length === 1) return nums[0];
 
-  let arr = [nums[0]];
+  let last = nums[0];
+  let max = nums[0];
 
   for (let i = 1, len = nums.length; i < len; i++) {
-    arr[i] = nums[i];
-    let sum = arr[i] + arr[i - 1];
-
-    if (sum > arr[i]) {
-      arr[i] = sum;
-    }
+    last = Math.max(nums[i], nums[i] + last);
+    max = Math.max(max, last);
   }
 
-  return Math.max.apply(null, arr);
+  return max;
 };
+// var maxSubArray = function (nums) {
+//   if (nums.length === 1) return nums[0];
+
+//   let arr = [nums[0]];
+
+//   for (let i = 1, len = nums.length; i < len; i++) {
+//     arr[i] = nums[i];
+//     let sum = arr[i] + arr[i - 1];
+
+//     if (sum > arr[i]) {
+//       arr[i] = sum;
+//     }
+//   }
+
+//   return Math.max.apply(null, arr);
+// };
