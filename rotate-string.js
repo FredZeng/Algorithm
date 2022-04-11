4fc537f4dbf06e8289c27801d91a5cd630864251
FredTsang <17154608+FredZeng@users.noreply.github.com>
2022-04-12 00:10:17 +0800

    feat: rotate-string

---
+++ rotate-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) {
+    return false;
+  }
+  return (s + s).indexOf(goal) > -1;
+};
